577a7f5756f76952af563404d43bcbad16126650
JerryG09 <phemmy09@gmail.com>
2019-11-30 18:31:34 +0100

    refactor

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App(): JSX.Element {
+  return (
+    <div>
+      <h1>Holy Mother</h1>
+      <p>Pick your favourite episode</p>
+    </div>
+  )
+}
+
+export default App;
