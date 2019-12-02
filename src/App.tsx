6a744e21550258c429084a6baf3077ae2bdd5318
JerryG09 <phemmy09@gmail.com>
2019-12-02 03:22:03 +0100

    fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import {Store} from './store'
 
 function App(): JSX.Element {
-  const store = useContext(Store)
+  const {state, dispatch} = useContext(Store)
+
+  useEffect(() => {
+    state.episodes.length === 0 && fetchData()
+    // return () => {
+    //   cleanup
+    // };
+  }, [])
+
+  const fetchData = async () => {
+    const URL = 'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes'
+    const data = await fetch(URL)
+    const dataJSON = await data.json()
+    return dispatch({
+      type: "FETCH_DATA",
+      payload: dataJSON._embedded.episodes
+    })
+  }
   return (
     <div>
-      {console.log(store)}
+      {console.log(state.episodes)}
       <h1>Holy Mother</h1>
       <p>Pick your favourite episode</p>
     </div>
