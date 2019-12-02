bf2cf8ceaee7fa96141a1d21722f8dc2f546da35
JerryG09 <phemmy09@gmail.com>
2019-12-02 01:55:34 +0100

    setup context

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {Store} from './store'
 
 function App(): JSX.Element {
+  const store = useContext(Store)
   return (
     <div>
+      {console.log(store)}
       <h1>Holy Mother</h1>
       <p>Pick your favourite episode</p>
     </div>
