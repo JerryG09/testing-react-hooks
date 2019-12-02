aa388f16b37e20373d34208586d86af4578ea9e1
JerryG09 <phemmy09@gmail.com>
2019-12-02 03:42:27 +0100

    display fetched data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
 import React, { useContext, useEffect } from 'react'
 import {Store} from './store'
 
+interface IEpisode {
+  id: number
+  url: string
+  name: string
+  season: number
+  number: number
+  airdate: string
+  airtime: string
+  airstamp: string
+  runtime: number
+  image: {medium: string, original: string}
+  summary: string
+}
+
 function App(): JSX.Element {
   const {state, dispatch} = useContext(Store)
 
@@ -9,7 +23,7 @@ function App(): JSX.Element {
     // return () => {
     //   cleanup
     // };
-  }, [])
+  })
 
   const fetchData = async () => {
     const URL = 'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes'
@@ -25,6 +39,19 @@ function App(): JSX.Element {
       {console.log(state.episodes)}
       <h1>Holy Mother</h1>
       <p>Pick your favourite episode</p>
+      <div className="">
+        {state.episodes.map((episode: IEpisode) => {
+          return (
+            <div className="" key={episode.id}>
+              {/* <img src={episode.image.medium} alt={`Rick and Mort ${episode.name}`} /> */}
+              <div className="">{episode.name}</div>
+              <div className="">
+                Season: {episode.season} Number: {episode.number}
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
