f27e4cce180aae053283b513fe6fb77fed7f3022
JerryG09 <phemmy09@gmail.com>
2019-12-02 01:54:52 +0100

    configure store provider in the App

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {StoreProvider} from './store'
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+<StoreProvider>
+  <App />
+</StoreProvider>, 
+  document.getElementById('root'));
