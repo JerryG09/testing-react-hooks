eff11c7f5779bea341950e4206f791dca2bf18f8
JerryG09 <phemmy09@gmail.com>
2019-12-03 04:11:45 +0100

    created episode list component

---
+++ src/EpisodeList.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { IEpisode } from './interfaces'
+
+function EpisodeList (props: any)  {
+  const { episodes, toggleFavAction, favourites} = props
+  return episodes.map((episode: IEpisode) => {
+    return (
+      <div className="episode-box" key={episode.id}>
+        {/* <img src={episode.image.medium} alt={`Rick and Mort ${episode.name}`}  /> */}
+        <div className="">{episode.name}</div>
+        <div className="">
+          <div className="" style={{ display: "flex", justifyContent: "space-between"}}>
+            Season: {episode.season} Number: {episode.number}
+          </div>
+          <button type="button" onClick={() => toggleFavAction(episode)}>
+            {favourites.find((fav: IEpisode) => fav.id === episode.id) ? "Unfav" : "Fav"}
+          </button>
+        </div>
+      </div>
+    )
+  })}
+
+
+export default EpisodeList
