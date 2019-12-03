ee4bd8cbd5b0a5e238e5323e560a65978982bd0c
JerryG09 <phemmy09@gmail.com>
2019-12-03 04:12:29 +0100

    backup

--- src/App.tsx
+++ src/App.tsx
@@ -51,20 +51,7 @@ function App(): JSX.Element {
         </div>
       </header>
       <div className="episode-layout">
-        {state.episodes.map((episode: IEpisode) => {
-          return (
-            <div className="episode-box" key={episode.id}>
-              {/* <img src={episode.image.medium} alt={`Rick and Mort ${episode.name}`}  /> */}
-              <div className="">{episode.name}</div>
-              <div className="">
-                <div className="">Season: {episode.season} Number: {episode.number}</div>
-                <button type="button" onClick={() => toggleFavAction(episode)}>
-                  {state.favourites.find((fav: IEpisode) => fav.id === episode.id) ? "Unfav" : "Fav"}
-                </button>
-              </div>
-            </div>
-          )
-        })}
+       
       </div>
     </div>
   )
