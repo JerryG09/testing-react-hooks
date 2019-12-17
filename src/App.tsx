6b00e0b3cadc381ca217f0660bfe96e0395003a0
JerryG09 <phemmy09@gmail.com>
2019-12-17 08:27:50 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect } from 'react';
+import { Link } from '@reach/router'
 import { Store } from './store'
 import { IAction, IEpisode, IEpisodeProps } from './interfaces';
 
@@ -57,7 +58,8 @@ function App(): JSX.Element {
           <p>Pick your favourite episode</p>
         </div>
         <div className="">
-          Favourite(s): {state.favourites.length}
+          <Link to='/'>Home</Link>
+          <Link to='/faves'>Favourite(s): {state.favourites.length}</Link>
         </div>
       </header>
       <React.Suspense fallback={<div>loading...</div>}>
