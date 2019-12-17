5961716a3f09eaf0e89237a6501a358013c4a9db
JerryG09 <phemmy09@gmail.com>
2019-12-17 09:51:05 +0100

    implemented reach router

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,20 @@ import {StoreProvider} from './store'
 import './index.css';
 import App from './App';
 
+import { Router, RouteComponentProps } from '@reach/router'
+
+import HomePage from './HomePage';
+import FavPage from './FavPage'
+
+const RouterPage = (props: {pageComponent: JSX.Element} & RouteComponentProps) => props.pageComponent
+
 ReactDOM.render(
 <StoreProvider>
-  <App />
-</StoreProvider>, 
+  <Router>
+    <App path='/'>
+      <RouterPage pageComponent={<HomePage />} path='/' />
+      <RouterPage pageComponent={<FavPage />} path='/favs' />
+    </App>
+  </Router>
+</StoreProvider>,
   document.getElementById('root'));
