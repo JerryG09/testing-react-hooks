0f799752fa438a427e48a92e1d04a4ba4ff78889
JerryG09 <phemmy09@gmail.com>
2019-12-17 12:11:30 +0100

    added  favouref items to list

--- src/FavPage.tsx
+++ src/FavPage.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Store } from './store';
+import { IEpisodeProps } from './interfaces';
+
+import { toggleFavAction } from './Actions'
+
+const EpisodeList = React.lazy(() => import('./EpisodeList'))
 
 function FavPage(): JSX.Element {
+  const { state, dispatch } = React.useContext(Store)
+
+  const props: IEpisodeProps = {
+    episodes: state.favourites,
+    store: { state, dispatch },
+    toggleFavAction,
+    favourites: state.favourites
+  };
+
   return (
-    <>
-      <h5>Fav page</h5>
-    </>
+    <React.Suspense fallback={<div>loading...</div>}>
+      <div className="">
+        <EpisodeList {...props} />
+      </div>
+    </React.Suspense>
   );
 }
 
