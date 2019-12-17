108805d59e05340bfbc697837a3f21583dd65866
JerryG09 <phemmy09@gmail.com>
2019-12-17 12:11:59 +0100

    refactored

--- src/EpisodeList.tsx
+++ src/EpisodeList.tsx
@@ -1,27 +1,34 @@
-import React from 'react'
-import { IEpisode } from './interfaces'
+import React from 'react';
+import { IEpisode } from './interfaces';
 
-function EpisodeList (props: any)  {
-  const { episodes, toggleFavAction, favourites, store} = props;
+function EpisodeList(props: any) {
+  const { episodes, toggleFavAction, favourites, store } = props;
 
-  const { state, dispatch } = store
+  const { state, dispatch } = store;
   return episodes.map((episode: IEpisode) => {
     return (
       <div className="episode-box" key={episode.id}>
-        <img src={episode.image.medium} alt={`Rick and Mort ${episode.name}`}  />
+        <img src={episode.image.medium} alt={`Rick and Mort ${episode.name}`} />
         <div className="">{episode.name}</div>
-        <div className="">
-          <div className="" style={{ display: "flex", justifyContent: "space-between"}}>
+        <div className=""
+            style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <div
+            className=""
+          >
             Season: {episode.season} Number: {episode.number}
           </div>
-          <button type="button" onClick={() => toggleFavAction(state, dispatch, episode)}>
-            {favourites.find((fav: IEpisode) => fav.id === episode.id) ? "Unfav" : "Fav"}
+          <button
+            type="button"
+            onClick={() => toggleFavAction(dispatch, state, episode)}
+          >
+            {favourites.find((fav: IEpisode) => fav.id === episode.id)
+              ? 'Unfav'
+              : 'Fav'}
           </button>
         </div>
       </div>
-    )
-  })
+    );
+  });
 }
 
-
-export default EpisodeList
+export default EpisodeList;
