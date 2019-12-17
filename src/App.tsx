7c3c8ba794909e2979fa5a8e42587284903343c8
JerryG09 <phemmy09@gmail.com>
2019-12-17 09:52:53 +0100

    refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,9 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { Link } from '@reach/router'
 import { Store } from './store'
-import { IAction, IEpisode, IEpisodeProps } from './interfaces';
 
-const EpisodeList = React.lazy(() => import('./EpisodeList'))
-
-function App(): JSX.Element {
-  const {state, dispatch} = useContext(Store)
-
-  useEffect(() => {
-    state.episodes.length === 0 && fetchData()
-    // return () => {
-    //   cleanup
-    // };
-  })
-
-  const fetchData = async () => {
-    const URL = 'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes'
-    const data = await fetch(URL)
-    const dataJSON = await data.json()
-    return dispatch({
-      type: "FETCH_DATA",
-      payload: dataJSON._embedded.episodes
-    })
-  }
-
-  const toggleFavAction = (episode: IEpisode): IAction => {
-    const episodeInFav = state.favourites.includes(episode)
-    let dispatchObj = {
-      type: "ADD_FAV",
-      payload: episode
-    }
-    if (episodeInFav) {
-      const favWithoutEpisode = state.favourites.filter((fav: IEpisode) => fav.id !== episode.id)
-      dispatchObj = {
-        type: "REMOVE_FAV",
-        payload: favWithoutEpisode
-      }
-    }
-    
-    return dispatch(dispatchObj)
-  }
-  console.log(state);
-
-  const props: IEpisodeProps = {
-    episodes: state.episodes,
-    toggleFavAction: toggleFavAction,
-    favourites: state.favourites
-  }
-
+function App(props: any): JSX.Element {
+  const {state} = useContext(Store)
 
   return (
     <div>
@@ -59,14 +14,10 @@ function App(): JSX.Element {
         </div>
         <div className="">
           <Link to='/'>Home</Link>
-          <Link to='/faves'>Favourite(s): {state.favourites.length}</Link>
+          <Link to='/favs'>Favourite(s): {state.favourites.length}</Link>
         </div>
       </header>
-      <React.Suspense fallback={<div>loading...</div>}>
-        <div className="episode-layout">
-        <EpisodeList {...props} />
-        </div>
-      </React.Suspense>
+      {props.children}
     </div>
   )
 }
