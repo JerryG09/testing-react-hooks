9e7044d77b2d2d2704000a130fc9c8c56b9cd694
JerryG09 <phemmy09@gmail.com>
2019-12-17 10:42:07 +0100

    refactored

--- src/EpisodeList.tsx
+++ src/EpisodeList.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { IEpisode } from './interfaces'
 
 function EpisodeList (props: any)  {
-  const { episodes, toggleFavAction, favourites} = props
+  const { episodes, toggleFavAction, favourites, store} = props;
+
+  const { state, dispatch } = store
   return episodes.map((episode: IEpisode) => {
     return (
       <div className="episode-box" key={episode.id}>
@@ -12,13 +14,14 @@ function EpisodeList (props: any)  {
           <div className="" style={{ display: "flex", justifyContent: "space-between"}}>
             Season: {episode.season} Number: {episode.number}
           </div>
-          <button type="button" onClick={() => toggleFavAction(episode)}>
+          <button type="button" onClick={() => toggleFavAction(state, dispatch, episode)}>
             {favourites.find((fav: IEpisode) => fav.id === episode.id) ? "Unfav" : "Fav"}
           </button>
         </div>
       </div>
     )
-  })}
+  })
+}
 
 
 export default EpisodeList
