9950bb7f30d4e7d9b96faf7ddfc5f0ea4e92ec66
JerryG09 <phemmy09@gmail.com>
2019-12-17 10:41:36 +0100

    refactored

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,49 +1,26 @@
 import React, { useEffect, useContext } from 'react';
 import { Store } from './store'
-import { IAction, IEpisode, IEpisodeProps } from './interfaces';
+import { IEpisodeProps } from './interfaces';
+import { fetchData, toggleFavAction } from './Actions'
+
 const EpisodeList = React.lazy(() => import('./EpisodeList'))
 
+
 function HomePage(): JSX.Element {
-  const {state, dispatch} = useContext(Store)
+  const { state, dispatch } = useContext(Store)
 
 
   useEffect(() => {
-    state.episodes.length === 0 && fetchData()
+    state.episodes.length === 0 && fetchData(dispatch)
     // return () => {
       //   cleanup
       // };
     })
 
-  const fetchData = async () => {
-    const URL = 'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes'
-    const data = await fetch(URL)
-    const dataJSON = await data.json()
-    return dispatch({
-      type: "FETCH_DATA",
-      payload: dataJSON._embedded.episodes
-    })
-  }
-
-  const toggleFavAction = (episode: IEpisode): IAction => {
-    const episodeInFav = state.favourites.includes(episode)
-    let dispatchObj = {
-      type: "ADD_FAV",
-      payload: episode
-    }
-    if (episodeInFav) {
-      const favWithoutEpisode = state.favourites.filter((fav: IEpisode) => fav.id !== episode.id)
-      dispatchObj = {
-        type: "REMOVE_FAV",
-        payload: favWithoutEpisode
-      }
-    }
-
-    return dispatch(dispatchObj)
-  }
-
   const props: IEpisodeProps = {
     episodes: state.episodes,
-    toggleFavAction: toggleFavAction,
+    store: { state, dispatch },
+    toggleFavAction,
     favourites: state.favourites
   }
 
@@ -58,4 +35,4 @@ function HomePage(): JSX.Element {
   )
 }
 
-export default HomePage
+export default HomePage;
